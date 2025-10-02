cb9def1eaa92ec0e2dda4fd921dc340a6816c5d7
Rafael Kremer <rafaelkremer.rs@gmail.com>
2025-10-02 19:06:27 -0300

    Alterações do jogo do Sonic - Ainda não está concluído

--- alunos/kremer/sonic/script.js
+++ alunos/kremer/sonic/script.js
@@ -1,4 +1,5 @@
 const sonic = document.querySelector(".sonic");
+const obstacle = document.querySelector(".obstacle");
 
 const jump = () => {
   sonic.classList.add("jump");
@@ -7,5 +8,24 @@ const jump = () => {
     sonic.classList.remove("jump");
   }, 500);
 };
+
+const loop = setInterval(() => {
+  const obstaclePosition = obstacle.offsetLeft;
+  const sonicPosition = +window
+    .getComputedStyle(sonic)
+    .bottom.replace("px", "");
+
+  if (obstaclePosition < 150 && sonicPosition < 80) {
+    obstacle.style.animation = "none";
+    obstacle.style.left = `${obstaclePosition}px`;
+
+    sonic.style.animation = "none";
+    sonic.style.left = `${sonicPosition}px`;
+
+    sonic.src = "../images/sonic_over.png";
+    sonic.style.width = "120px";
+    sonic.style.marginLeft = "";
+  }
+}, 10);
 
 document.addEventListener("keydown", jump);
